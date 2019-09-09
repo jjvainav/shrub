057c35e6916949d94ba5701ca7d629669dc96f63
Jeremy Vainavicz <jjvainav@gmail.com>
2019-09-09 10:02:18 -0400

    Deleted vue-browser and vue-core - those are no superseded by vue and vue-server.

--- packages/vue-browser/types/vue-shims.d.ts
+++
@@ -1,5 +0,0 @@
-// this declaration is needed to import .vue files in the .ts scripts.
-declare module "*.vue" {
-    import Vue from "vue";
-    export default Vue;
-}
--- packages/vue-core/src/index.ts
+++
@@ -1,5 +0,0 @@
-﻿export { IComponent, IComponentCollection } from "./component";
-export * from "./component-decorator";
-export * from "./component-service";
-export * from "./model-service";
-export * from "./module";
