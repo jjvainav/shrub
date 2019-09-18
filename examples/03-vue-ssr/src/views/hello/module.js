325701273da81944e9c17a84064480d78238f124
Jeremy Vainavicz <jjvainav@gmail.com>
2019-09-18 07:53:18 -0400

    Fixed issue in vue-ssr example and added script to the workbench example.

--- examples/03-vue-ssr/src/views/hello/module.js
+++ examples/03-vue-ssr/src/views/hello/module.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import { IVueConfiguration, VueModule } from "@shrub/vue";
 
 export default class HelloModule {
-    name = "hello";
-    dependencies = [VueModule];
-
+    constructor() {
+        this.name = "hello";
+        this.dependencies = [VueModule];
+    }
+    
     configure({ config }) {
         config.get(IVueConfiguration).mount(Vue.extend({ 
-            template: "<h1>Hello Vue!</h1>" 
+            template: '<div id="app"><h1>Hello Vue!</h1></div>' 
         }));
     }
 }
