9cd46f791340d1346c555c067b1206d6dfb8c305
Jeremy Vainavicz <jjvainav@gmail.com>
2020-06-10 15:42:26 -0400

    Removed some commented out stuff.

--- packages/messaging/src/message.ts
+++ packages/messaging/src/message.ts
@@ -9,29 +9,6 @@ export interface IMessage {
     readonly data: any;
 }
 
-// TODO: update/fix event-stream stuff
-// TODO: pass ILogger as an option when calling subscribe()
-
-// TODO: in event have an event-id metadata - this is the id assigned to the event for event sourcing... or call it edit-id instead and have that part of the data?
-// TODO: is the event id in QK even being used?
-
-
-// export interface IMessageDetails {
-//     /** A set of message headers. */
-//     readonly headers?: MessageHeaders;
-//     /** The message payload. */
-//     readonly body: any;
-// }
-
-// export interface IMessage {
-//     /** A unique id assigned to the message. */
-//     readonly id: string;
-//     /** A set of message headers. */
-//     readonly headers: MessageHeaders;
-//     /** The message payload. */
-//     readonly body: any;
-// }
-
 export namespace Message {
     export function isMessage(message: any): message is IMessage {
         return typeof (<IMessage>message).id === "string" && (<IMessage>message).metadata !== undefined && (<IMessage>message).data !== undefined;
