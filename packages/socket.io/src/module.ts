fb4b86adbc795105cfc7382d30501223fc51424e
Jeremy Vainavicz <jjvainav@gmail.com>
2021-05-28 10:21:50 -0400

    Updated minimum node requirement and upgraded socket.io.

--- packages/socket.io/src/module.ts
+++ packages/socket.io/src/module.ts
@@ -1,13 +1,13 @@
-import io from "socket.io";
+import { Server } from "socket.io";
 import { createConfig, createService, IModule, IModuleInitializer, IServiceRegistration } from "@shrub/core";
 import { HttpModule, IHttpServer } from "@shrub/http";
 
 export const ISocketIOConfiguration = createConfig<ISocketIOConfiguration>();
-export interface ISocketIOConfiguration extends io.Server {
+export interface ISocketIOConfiguration extends Server {
 }
 
 export const ISocketIOServer = createService<ISocketIOServer>("socket.io-server");
-export interface ISocketIOServer extends io.Server {
+export interface ISocketIOServer extends Server {
 }
 
 /** A module that creates and exposes a socket.io server attached to the currently registered http server. */
@@ -20,6 +20,6 @@ export class SocketIOModule implements IModule {
     }
 
     configureServices(registration: IServiceRegistration): void {
-        registration.registerSingleton(ISocketIOServer, { create: services => io(services.get(IHttpServer)) });
+        registration.registerSingleton(ISocketIOServer, { create: services => new Server(services.get(IHttpServer)) });
     }
 }
