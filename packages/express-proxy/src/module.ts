af39373c4760667fdd6f791b9b7ed9da69deb081
Jeremy Vainavicz <jjvainav@gmail.com>
2021-10-23 08:50:35 -0400

    Updated the express proxy package to utilize some recent changes.

--- packages/express-proxy/src/module.ts
+++ packages/express-proxy/src/module.ts
@@ -1,26 +1,21 @@
-import { createConfig, IModule, IModuleInitializer, IServiceRegistration } from "@shrub/core";
+import { createConfig, IModule, IModuleInitializer } from "@shrub/core";
 import { ExpressModule } from "@shrub/express";
-import { IProxyFactory, IProxyRegistrationService, IProxyService, IProxyType, ProxyRegistrationService, ProxyService } from "./proxy";
+import { IProxyType } from "./proxy";
 
 export const IExpressProxyConfiguration = createConfig<IExpressProxyConfiguration>();
 export interface IExpressProxyConfiguration {
     /** Registers a proxy type and factory. */
-    useProxy<T>(proxyType: IProxyType<T>, factory: IProxyFactory<T>): void;
+    useProxy<T>(proxyType: IProxyType<T>, factory: () => T): void;
 }
 
-/** Express module that provides support for registering and using request proxies. */
+/** Express module that provides support for configuring proxies. */
 export class ExpressProxyModule implements IModule {
     readonly name = "express-proxy";
     readonly dependencies = [ExpressModule];
 
     initialize(init: IModuleInitializer): void {
-        init.config(IExpressProxyConfiguration).register(({ services }) => ({
-            useProxy: (proxyType, factory) => services.get(IProxyRegistrationService).registerProxy(proxyType.key, factory)
+        init.config(IExpressProxyConfiguration).register(() => ({
+            useProxy: (proxyType, factory) => proxyType.configure({ factory })
         }));
-    }
-
-    configureServices(registration: IServiceRegistration): void {
-        registration.register(IProxyRegistrationService, ProxyRegistrationService);
-        registration.register(IProxyService, ProxyService);
     }
 }
--- packages/express-proxy/src/proxy.ts
+++ packages/express-proxy/src/proxy.ts
@@ -1,21 +1,8 @@
-import { createInjectable, createService, IInjectable, IServiceCollection, Singleton, Transient } from "@shrub/core";
-import { IRequestContext } from "@shrub/express";
+import { createInjectable, IInjectable } from "@shrub/core";
+import { ControllerInvokerConstructor, IControllerInvokerService, IRequestContext } from "@shrub/express";
 import client, { IRequest, IRequestClient } from "@sprig/request-client";
 import { RequestClientConstructor } from "@sprig/request-client-class";
-import { Request, RequestHandler, Response } from "express";
-
-export interface IExpressRequestOptions {
-    readonly context: IRequestContext;
-    readonly handler: RequestHandler;
-    readonly body?: any;
-    readonly params?: any;
-    readonly query?: any;
-}
-
-export interface IExpressResponse {
-    readonly status: number;
-    readonly data: any;
-}
+import { RequestHandler } from "express";
 
 /** A callback responsible for preparing a request to be invoked on behalf of a current request context. */
 export interface IPrepareClientRequest {
@@ -26,128 +13,51 @@ export interface IPrepareClientRequest {
 export interface IProxyType<T> extends IInjectable<T> {
 }
 
-/** Factory function for creating proxy instances. */
-export interface IProxyFactory<T> {
-    (services: IServiceCollection): T;
+/** Defines options for a remote proxy. */
+export interface IRemoteProxyOptions {
+    readonly url: string;
+    readonly prepareRequest?: IPrepareClientRequest;
+    readonly prepareStream?: IPrepareClientRequest;
 }
-
-/** A service for registering and managing proxies. */
-export interface IProxyService {
-    getProxy<T>(key: string): T;
-}
-
-/** @internal */
-export interface IProxyRegistrationService {
-    getFactory<T>(key: string): IProxyFactory<T> | undefined;
-    registerProxy<T>(key: string, factory: IProxyFactory<T>): void;
-}
-
-export const IProxyService = createService<IProxyService>("proxy-service");
-/** @internal */
-export const IProxyRegistrationService = createService<IProxyRegistrationService>("proxy-registration-service");
 
 /** Creates an injectable proxy used to define proxy interfaces and can be used as a decorator to inject proxies into service instances. */
 export function createProxyType<T>(key: string): IProxyType<T> {
-    return createInjectable({ key, factory: services => services.get(IProxyService).getProxy<T>(key) });
+    return createInjectable({ 
+        key, 
+        configurable: true,
+        factory: () => { throw new Error(`Proxy for key (${key}) not registered, proxy types must be registered with the express-proxy module to be used.`); }
+    });
 }
 
 /** Base class for a local proxy where the API module is hosted in the same process. */
-export abstract class LocalProxy {
-    /** Invokes an express request handler directly and returns a result. */
-    protected invokeRequest(options: IExpressRequestOptions): Promise<IExpressResponse> {
-        return new Promise((resolve, reject) => options.handler(
-            this.createExpressRequest(options),
-            this.createExpressResponse(resolve),
-            (reason: any) => {
-                if (reason) {
-                    // assume next is only invoked if there is an error
-                    reject(reason);
-                }
-            }));
+export abstract class LocalProxy<TClient> {
+    constructor(
+        private readonly controllerInvokerConstructor: ControllerInvokerConstructor<TClient>,
+        private readonly handler?: RequestHandler) {
     }
 
-    private createExpressRequest(options: IExpressRequestOptions): Request {
-        return <Request><unknown>{
-            context: options.context,
-            handler: options.handler,
-            body: options.body,
-            params: options.params,
-            query: options.query
-        };
-    }
-
-    private createExpressResponse(callback: (response: IExpressResponse) => void): Response {
-        let status = 200;
-        return <Response>{
-            json: function (body): Response {
-                return this.send(body);
-            },
-            send: function (body): Response {
-                callback({ status, data: body });
-                return this;
-            },
-            sendStatus: function (s): Response {
-                status = s;
-                return this.send();
-            },
-            status: function (s): Response {
-                status = s;
-                return this;
-            }
-        };
+    protected createClient(context: IRequestContext): TClient {
+        return context.services.get(IControllerInvokerService).createControllerInvoker(this.controllerInvokerConstructor, this.handler);
     }
 }
 
 /** Base class for a remote proxy where the API is accessed via an external REST endpoint. */
 export abstract class RemoteProxy<TClient> {
     constructor(
-        private readonly url: string, 
         private readonly requestClientConstructor: RequestClientConstructor<TClient>,
-        private readonly prepareRequest?: IPrepareClientRequest,
-        private readonly prepareStream?: IPrepareClientRequest) {
+        private readonly options: IRemoteProxyOptions) {
     }
 
     /** Creates a new request client class instance for the proxy. */
     protected createClient(context: IRequestContext): TClient {
-        const prepareRequest = this.prepareRequest;
-        const prepareStream = this.prepareStream;
+        const prepareRequest = this.options.prepareRequest;
+        const prepareStream = this.options.prepareStream;
         
         const requestClient: IRequestClient = {
             request: prepareRequest && (options => prepareRequest(context, client.request(options))) || client.request,
             stream: prepareStream && (options => prepareStream(context, client.stream(options))) || client.stream,
         };
 
-        return new this.requestClientConstructor({ url: this.url, client: requestClient });
-    }
-}
-
-/** @internal */
-@Transient
-export class ProxyService implements IProxyService {
-    constructor(@IServiceCollection private readonly services: IServiceCollection) {
-    }
-
-    getProxy<T>(key: string): T {
-        const factory = this.services.get(IProxyRegistrationService).getFactory<T>(key);
-
-        if (!factory) {
-            throw new Error(`Proxy for key (${key}) not registered, proxy types must be registered with the express-proxy module to be used.`);
-        }
-
-        return factory(this.services);
-    }
-}
-
-/** @internal */
-@Singleton
-export class ProxyRegistrationService implements IProxyRegistrationService {
-    private readonly proxies = new Map<string, IProxyFactory<any>>();
-
-    getFactory<T>(key: string): IProxyFactory<T> | undefined {
-        return this.proxies.get(key);
-    }
-
-    registerProxy<T>(key: string, factory: IProxyFactory<T>): void {
-        this.proxies.set(key, factory);
+        return new this.requestClientConstructor({ url: this.options.url, client: requestClient });
     }
 }
