62788b064b4d1c4eb7b6b935a917b955af25aad1
Jeremy Vainavicz <jjvainav@gmail.com>
2021-11-10 13:46:34 -0500

    Updated examples.

--- examples/09-vue-3-ssr/src/controllers/app-controller.ts
+++ examples/09-vue-3-ssr/src/controllers/app-controller.ts
@@ -2,7 +2,7 @@ import { Route, Get } from "@shrub/express";
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "./base-controller";
 
-@Route()
+@Route("*")
 export class AppController extends BaseController {
     @Get()
     get(req: Request, res: Response, next: NextFunction): void {
